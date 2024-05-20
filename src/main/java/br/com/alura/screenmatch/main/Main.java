6160502a2df7f23641d2564d2c133756baf235d6
malursilva <malursilva1712@gmail.com>
2024-05-20 15:06:35 -0300

    added streams to filter data shown

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -1,13 +1,16 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.model.EpisodeData;
 import br.com.alura.screenmatch.model.SeasonData;
 import br.com.alura.screenmatch.model.SeriesData;
 import br.com.alura.screenmatch.service.ApiConsuption;
 import br.com.alura.screenmatch.service.DataConverter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     private final String ADDRESS = "https://www.omdbapi.com/?t=";
@@ -25,11 +28,20 @@ public class Main {
         SeriesData seriesData = converter.getData(json, SeriesData.class);
         System.out.println(seriesData);
 
-        List<SeasonData> seasonDataList = new ArrayList<>();
+        List<SeasonData> seasonsList = new ArrayList<>();
 		for (int i=1; i <= seriesData.totalSeasons(); i++) {
-			json = apiConsuption.getData(ADDRESS + serieName + "&season=" + i + API_KEY);
-			seasonDataList.add(converter.getData(json, SeasonData.class));
-		}
-		seasonDataList.forEach(System.out::println);
+			json = apiConsuption.getData(ADDRESS + serieName + "&season=" +i + API_KEY);
+			seasonsList.add(converter.getData(json, SeasonData.class));
+        }
+        List<EpisodeData> allEpisodesData = new ArrayList<>();
+        allEpisodesData = seasonsList.stream().flatMap(t -> t.episodes().stream()).collect(Collectors.toList());
+        System.out.println("\n -- Top 5 Episódios --");
+        allEpisodesData.stream()
+                .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(EpisodeData::rating).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+
     }
 }
